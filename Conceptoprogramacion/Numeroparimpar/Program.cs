f818c1fd69ac6bb9ebb1c02534bfa322be6aedd4
28samu <samuel_f28@hotmail.com>
2025-06-05 15:26:52 -0500

    creacionproyectoparimpar

---
+++ Conceptoprogramacion/Numeroparimpar/Program.cs
@@ -0,0 +1,17 @@
+﻿do
+{
+    // See https://aka.ms/new-console-template for more information
+    Console.Write("ingrese un numero:");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.Write("el numero es par");
+
+    }
+    else
+    {
+        Console.Write("el numero es inpar");
+    }
+
+} while (true); 
